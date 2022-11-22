b4ed9576c191917405c5056236caf65e6c1c55cc
Dung <2051012015dung@ou.edu.vn>
2022-11-23 05:06:21 +0700

    cap nhat giao dien dang nhap va db

--- QuanLyKhoHang(1)/src/main/java/com/mycompany/quanlykhohang/App.java
+++ QuanLyKhoHang(1)/src/main/java/com/mycompany/quanlykhohang/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
@@ -17,7 +18,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FXMLdashboard"), 640, 480);
+        scene = new Scene(loadFXML("FXMLLogin"), 520, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- QuanLyKhoHang(1)/src/main/java/com/mycompany/quanlykhohang/FXMLLoginController.java
+++ QuanLyKhoHang(1)/src/main/java/com/mycompany/quanlykhohang/FXMLLoginController.java
@@ -7,11 +7,22 @@ package com.mycompany.quanlykhohang;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * FXML Controller class
  *
- * @author DELL
+ * @author xelan
  */
 public class FXMLLoginController implements Initializable {
 
@@ -20,9 +31,56 @@ public class FXMLLoginController implements Initializable {
      * @param url
      * @param rb
      */
+    
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField pswPassword;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    //TODO
+    }
     
+    public void loginButtonOnAction(ActionEvent e) throws SQLException {
+        
+        if (txtUsername.getText().isBlank() == false && pswPassword.getText().isBlank() == false){
+            //loginMessageLabel.setText("Vui lòng điền thông tin đăng nhập chính xác!"); //Test Login Message
+            validateLogin();
+        }
+        else
+            loginMessageLabel.setText("Vui lòng nhập tên đăng nhập và mật khẩu.");            
+    }
+    
+    public void btnCancelOnAction(ActionEvent e) {
+        Stage stage = (Stage) btnCancel.getScene().getWindow();
+        stage.close();
+    }
+    
+    public void validateLogin() throws SQLException {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        
+        String veryfyLogin = "SELECT count(1) FROM quanlykhohangdb.nhanvien WHERE tenDN = '" + txtUsername.getText() + "' AND matKhau = '" + pswPassword.getText() + "'";
+        
+        try{
+            
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(veryfyLogin);
+            
+            while(queryResult.next()) {
+                if(queryResult.getInt(1) == 1){
+                    loginMessageLabel.setText("Đăng nhập thành công!");
+                } else {
+                    loginMessageLabel.setText("Tên đăng nhập hoặc mật khẩu không hợp lệ.");
+                }
+            }
+        }   catch (Exception e){
+                e.printStackTrace();
+            }
+    } 
 }
